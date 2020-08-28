ceb01e71c511631518fb78f8b078e29daabfeddf
unknown <samody2006@yahoo.com>
2020-08-28 07:51:49 +0100

    API

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        factory(App\Student::class,20)->create();
+        factory(App\User::class,20)->create();
+    }
+}
